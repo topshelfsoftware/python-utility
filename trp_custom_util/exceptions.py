00bf322eddd7f24b32917ca9f6131483d017c707
Tanner Pubins <t.pubins@icloud.com>
2023-02-03 20:14:22 -0700

    Create exceptions module for defining custom exceptions.

---
+++ trp_custom_util/exceptions.py
@@ -0,0 +1,9 @@
+"""Custom exceptions defined here."""
+
+class ModuleError(RuntimeError):
+    """Raised to indicate a module failure."""
+    def __init__(self, value):
+        self.value = value
+    
+    def __str__(self):
+        return(repr(self.value))
