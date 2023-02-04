ca864e1bc98ac26e87d8d4566b0e033b6b9013a8
Tanner Pubins <t.pubins@icloud.com>
2023-02-03 21:07:09 -0700

    Add delay and is_tool functions to common module.

--- trp_custom_util/common.py
+++ trp_custom_util/common.py
@@ -1,6 +1,8 @@
 """Helper functions for common programmatic actions."""
 
 import json
+import shutil
+import time
 from typing import List, Union
 
 from trp_custom_util.log import get_logger
@@ -12,8 +14,19 @@ def fmt_json(input: dict) -> str:
     return json.dumps(input, default=str)
 
 
+def delay(sec: float):
+    """Delay program execution by some number of seconds."""
+    logger.debug(f"delay {sec}s")
+    time.sleep(sec)
+
+
 def unique(values: Union[List[str], List[int]]):
     """Generate a unique list of values."""
     unique_values = list(set(values))
     logger.debug(f"unique values: {unique_values}")
     return unique_values
+
+
+def is_tool(name: str) -> bool:
+    """Check whether `name` is on PATH and marked as executable."""
+    return shutil.which(name) is not None
