970b0eb324c490fb483fd8aaf058f99db9be16f6
Tanner Pubins <t.pubins@icloud.com>
2023-06-27 17:42:54 -0700

    Make logging and exception handling more specific.

--- topshelfsoftware_util/aws.py
+++ topshelfsoftware_util/aws.py
@@ -2,6 +2,7 @@
 
 from boto3.session import Session as Boto3Session
 from botocore.client import BaseClient
+from botocore.exceptions import ClientError as BotoClientError
 
 from topshelfsoftware_util.common import fmt_json
 from topshelfsoftware_util.log import get_logger
@@ -20,14 +21,14 @@ def create_boto3_client(service_name: str, region: str = None) -> BaseClient:
         Region where service abides.
         Default is `None`.
     """
+    logger.debug(f"creating boto3 client: {service_name}")
     try:
-        logger.debug(f"creating boto3 client: {service_name}")
         session = Boto3Session(region_name=region)
         client = session.client(service_name=service_name)
-        logger.debug("boto3 client successfully created")
-    except Exception as e:
+    except BotoClientError as e:
         logger.error(f"failed to create client: {service_name}. Reason: {e}")
         raise e
+    logger.debug("boto3 client successfully created")
     return client
 
 
@@ -43,19 +44,21 @@ def get_ssm_value(name: str, region: str = None) -> str:
         Region where service abides.
         Default is `None`.
     """
+    logger.debug(f"getting ssm: {name}")
     try:
         ssm_client = create_boto3_client(service_name="ssm", region=region)
-        logger.debug(f"getting ssm: {name}")
-
         ssm_resp = ssm_client.get_parameter(Name=name)
         logger.debug(f"resp: {fmt_json(ssm_resp)}")
-
-        val = ssm_resp['Parameter']['Value']
-        logger.debug(f"return: {val}")
-        return val
-    except Exception as e:
+        val = ssm_resp["Parameter"]["Value"]
+    except BotoClientError as e:
         logger.error(f"failed to retrieve ssm parameter: {name}. Reason: {e}")
         raise e
+    except KeyError as e:
+        logger.error(f"failed to retrieve parameter value from ssm response: {name}. Reason: {e}")
+        raise e
+    logger.debug(f"ssm parameter value: {val}")
+    return val
+    
 
 
 def get_secret_value(secret_id: str, region: str = None) -> str:
@@ -70,16 +73,17 @@ def get_secret_value(secret_id: str, region: str = None) -> str:
         Region where service abides.
         Default is `None`.
     """
+    logger.debug(f"getting secret: {secret_id}")
     try:
         secret_client = create_boto3_client(service_name="secretsmanager", region=region)
-        logger.debug(f"getting secret: {secret_id}")
-
         secret_resp = secret_client.get_secret_value(SecretId=secret_id)
         logger.debug(f"resp: {fmt_json(secret_resp)}")
-
         val = secret_resp["SecretString"]
-        logger.debug("return: <redacted>")
-        return val
-    except Exception as e:
+    except BotoClientError as e:
         logger.error(f"failed to retrieve secret: {secret_id}. Reason: {e}")
         raise e
+    except KeyError as e:
+        logger.error(f"failed to retrieve string from secretsmanager response: {secret_id}. Reason: {e}")
+        raise e
+    logger.debug("secret string: <redacted>")
+    return val
