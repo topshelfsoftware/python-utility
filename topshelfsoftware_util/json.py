0c4b9e55831d21fba9c5ca3c4d5733452d5af9e1
Tanner Pubins <t.pubins@icloud.com>
2024-07-15 12:08:45 -0700

    Unit test the json module. Deprecate the load_json_schema func.

--- topshelfsoftware_util/json.py
+++ topshelfsoftware_util/json.py
@@ -1,6 +1,9 @@
 """Work with JSON data."""
 
 import json
+from pathlib import Path
+from typing import Union
+import warnings
 
 from topshelfsoftware_util.log import get_logger
 
@@ -8,14 +11,63 @@ logger = get_logger(__name__, stream=None)
 
 
 def fmt_json(input: dict) -> str:
-    """Return formatted json as a string."""
+    """Return formatted JSON as a string.
+
+    Parameters
+    ----------
+    input: dict
+        JSON input to format.
+
+    Returns
+    ----------
+    str
+        The formatted JSON.
+    """
     return json.dumps(input, default=str)
 
 
-def load_json_schema(file: str) -> dict:
-    """Loads a JSON schema file into a dictionary."""
-    logger.info(f"Reading schema file: {file}")
+def load_json(file: Union[str, Path]) -> dict:
+    """Loads a JSON file into a dictionary.
+
+    Parameters
+    ----------
+    file: str | Path
+        Path to JSON file.
+
+    Returns
+    ----------
+    dict
+        JSON content as a dict.
+    """
+    logger.debug(f"Reading JSON file: {file}")
     with open(file, "r") as f:
-        schema: dict = json.load(f)
-        logger.debug(f"schema: {fmt_json(schema)}")
+        dictionary: dict = json.load(f)
+        logger.debug(f"dict: {fmt_json(dictionary)}")
+    return dictionary
+
+
+def load_json_schema(file: str) -> dict:
+    """Loads a JSON schema file into a dictionary.
+
+    NOTE: Deprecated function, use `load_json` instead.
+
+    Parameters
+    ----------
+    file: str
+        Path to JSON file.
+
+    Returns
+    ----------
+    dict
+        JSON content as a dict.
+    """
+    deprecation_msg = (
+        "load_json_schema function is deprecated and will be removed "
+        "in a future release. Use load_json instead."
+    )
+    warnings.warn(deprecation_msg, DeprecationWarning, stacklevel=2)
+    logger.warning(deprecation_msg)
+
+    logger.info(f"Reading schema file: {file}")
+    schema = load_json(file)
     return schema
